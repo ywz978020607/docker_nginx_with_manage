31272e8cdb4fc844fa18132b4ea761244a52f68f
ywzsunny <ywzsunny@buaa.edu.cn>
2023-03-04 14:25:56 +0800

    add aliyun host

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 
 apt-get update -y
 # apt-get -y install sshpass
-pip3 install -r /tmp/requirements.txt
+pip3 install -r /tmp/requirements.txt -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 
 
 # export PATH = xx
